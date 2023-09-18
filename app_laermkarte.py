dd1b33c67843cc8427b0d18bf9a156ffe987e383
NoahPannicke <50919321+NoahPannicke@users.noreply.github.com>
2023-09-18 18:10:55 +0200

    Update app_laermkarte.py

--- app_laermkarte.py
+++ app_laermkarte.py
@@ -5,13 +5,13 @@ from functions_laermkarte import map_func
 app = Flask(__name__)
 @app.route('/')
 def my_form():
-    return render_template('laermkartelaermkarte.html')
+    return render_template('laermkarte.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     uhrzeit = str(request.form['variable'])
-    return test_app.map_func(uhrzeit)
+    return functions_laermkarte.map_func(uhrzeit)
 @app.route('/foo', methods=['POST', 'GET'])
 def index():
-    return test_app.hist_map_func()
+    return functions_laermkarte.hist_map_func()
 
