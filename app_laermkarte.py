4c791f826fdd7156bd72f1b9646999ac765c38bf
NoahPannicke <50919321+NoahPannicke@users.noreply.github.com>
2023-09-19 15:41:21 +0200

    Final App Files

---
+++ app_laermkarte.py
@@ -0,0 +1,21 @@
+## to execute ##
+#python -m flask --app .\app_test.py run
+################
+import functions_laermkarte
+from flask import Flask
+from flask import request, render_template
+app = Flask(__name__)
+@app.route('/')
+def my_form():
+    return render_template('laermkarte.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    uhrzeit = str(request.form['variable'])
+    return functions_laermkarte.map_func(uhrzeit)
+@app.route('/foo', methods=['POST', 'GET'])
+def index():
+    return functions_laermkarte.hist_map_func()
+if __name__ == "__main__":
+    app.run(debug=True)
+
